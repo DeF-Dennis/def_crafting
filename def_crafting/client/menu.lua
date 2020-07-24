fc7c5474f2f1f7503517f744fe665673800b369e
DeF <65823098+DeF-Dennis@users.noreply.github.com>
2020-07-24 17:58:09 +0200

    Add files via upload

---
+++ def_crafting/client/menu.lua
@@ -0,0 +1,211 @@
+------------------------------
+----------- By DeF -----------
+---- Easy Crafting System ----
+------------------------------
+
+Citizen.CreateThread(function()
+    WarMenu.CreateMenu('craft', "Crafting")
+    WarMenu.SetSubTitle('craft', "Manufacturing of goods")
+	
+    WarMenu.CreateSubMenu('inv1', 'craft', 'Weapons')
+    WarMenu.CreateSubMenu('inv2', 'craft', 'Items')
+    WarMenu.CreateSubMenu('inv3', 'craft', 'Tools')
+    WarMenu.CreateSubMenu('inv4', 'craft', 'Ammo')
+
+
+    while true do
+
+        local ped = GetPlayerPed()
+        local coords = GetEntityCoords(PlayerPedId())
+		
+        if WarMenu.IsMenuOpened('craft') then
+		
+		if WarMenu.MenuButton('Weapons', 'inv1') then
+            end
+        if WarMenu.MenuButton('Items', 'inv2') then
+            end
+        if WarMenu.MenuButton('Tools', 'inv3') then
+            end
+        if WarMenu.MenuButton('Ammo', 'inv4') then
+            end
+
+            WarMenu.Display()
+        elseif WarMenu.IsMenuOpened('inv1') then
+
+			if WarMenu.Button("Craft Bow") then
+			TriggerServerEvent("def_crafting:getbow", "bow_w", "WEAPON_BOW")
+			WarMenu.CloseMenu()
+			
+            elseif WarMenu.Button("Craft Knife") then
+              TriggerServerEvent("def_crafting:getknife", "knife_w", "WEAPON_MELEE_KNIFE")
+            WarMenu.CloseMenu()
+
+            elseif WarMenu.Button("Craft Throwing knife") then
+              TriggerServerEvent("def_crafting:getthrowing", "throwing_w", "WEAPON_THROWN_THROWING_KNIVES")
+            WarMenu.CloseMenu()
+            end
+
+
+            WarMenu.Display()
+        elseif WarMenu.IsMenuOpened('inv2') then
+
+            if WarMenu.Button("Craft Knife blade") then
+                TriggerServerEvent("def_crafting:getknifeblade")
+            WarMenu.CloseMenu()
+        
+            elseif WarMenu.Button("Craft handle") then
+                TriggerServerEvent("def_crafting:gethandle")
+            WarMenu.CloseMenu()
+            
+            elseif WarMenu.Button("Craft fishinghook") then
+                TriggerServerEvent("def_crafting:getfishinghook")
+            WarMenu.CloseMenu()
+
+            elseif WarMenu.Button("Craft arrowhead") then
+                TriggerServerEvent("def_crafting:getarrowhead")
+            WarMenu.CloseMenu()
+            end
+
+            WarMenu.Display()
+        elseif WarMenu.IsMenuOpened('inv3') then
+
+            if WarMenu.Button("Craft fishingrod") then
+                TriggerServerEvent("def_crafting:getfishingrod")
+            WarMenu.CloseMenu()
+            end
+
+            WarMenu.Display()
+        elseif WarMenu.IsMenuOpened('inv4') then
+
+            if WarMenu.Button("Craft arrows") then
+                TriggerServerEvent("def_crafting:getarrow")
+            WarMenu.CloseMenu()
+            end
+
+
+            WarMenu.Display()
+
+        elseif IsControlJustReleased(1, 0x4CC0E2FE) then -- Use B to open the Menu
+           WarMenu.OpenMenu('craft')
+        end
+		
+	
+        Citizen.Wait(0)
+    end
+end)
+
+-- If you want to open the Menu by a Command --
+
+--[[RegisterCommand("craft", function(source, args, rawCommand) -- craft COMMAND
+local _source = source
+WarMenu.OpenMenu('craft')
+end, false)]]--
+
+-- {Settings ProgressBars} --  
+
+-- {Weapons}
+
+RegisterNetEvent('def_crafting:getbow')
+AddEventHandler('def_crafting:getbow', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 35000, true, false, false, false)
+    exports['progressBars']:startUI(35000, "Crafting bow...")
+    Citizen.Wait(35000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+RegisterNetEvent('def_crafting:getknife')
+AddEventHandler('def_crafting:getknife', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 35000, true, false, false, false)
+    exports['progressBars']:startUI(35000, "Crafting knife...")
+    Citizen.Wait(35000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+RegisterNetEvent('def_crafting:getthrowing')
+AddEventHandler('def_crafting:getthrowing', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 35000, true, false, false, false)
+    exports['progressBars']:startUI(35000, "Crafting throwing knife...")
+    Citizen.Wait(35000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+-- Gegenstände
+
+RegisterNetEvent('def_crafting:getknifeblade')
+AddEventHandler('def_crafting:getknifeblade', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 32500, true, false, false, false)
+    exports['progressBars']:startUI(32500, "Crafting knifeblade...")
+    Citizen.Wait(32500)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+RegisterNetEvent('def_crafting:gethandle')
+AddEventHandler('def_crafting:gethandle', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 25000, true, false, false, false)
+    exports['progressBars']:startUI(25000, "Crafting handle...")
+    Citizen.Wait(25000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+RegisterNetEvent('def_crafting:getfishinghook')
+AddEventHandler('def_crafting:getfishinghook', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 20000, true, false, false, false)
+    exports['progressBars']:startUI(20000, "Crafting fishinghook...")
+    Citizen.Wait(20000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+RegisterNetEvent('def_crafting:getarrowhead')
+AddEventHandler('def_crafting:getarrowhead', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 27000, true, false, false, false)
+    exports['progressBars']:startUI(27000, "Crafting arrowhead...")
+    Citizen.Wait(27000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+-- {Tools} --
+
+RegisterNetEvent('def_crafting:getfishingrod')
+AddEventHandler('def_crafting:getfishingrod', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 30000, true, false, false, false)
+    exports['progressBars']:startUI(30000, "Crafting fishingrod...")
+    Citizen.Wait(30000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
+
+-- {Ammo} --
+
+RegisterNetEvent('def_crafting:getarrow')
+AddEventHandler('def_crafting:getarrow', function()
+
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 31000, true, false, false, false)
+    exports['progressBars']:startUI(31000, "Crafting Arrow...")
+    Citizen.Wait(31000)
+    ClearPedTasksImmediately(PlayerPedId())
+
+end)
---
+++ def_crafting/server/server.lua
@@ -0,0 +1,226 @@
+------------------------------
+----------- By DeF -----------
+---- Easy Crafting System ----
+------------------------------
+
+local data = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    data = call
+end)
+
+
+-- {Weapons} --
+
+RegisterServerEvent('def_crafting:getbow')
+AddEventHandler("def_crafting:getbow", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "wood")
+        local count2 = data.checkItem(_source, "string")
+		if count >= 2 and count2 >= 3 then
+		
+			data.delItem(_source,"wood", 2)
+            data.delItem(_source,"string",3)
+			
+          TriggerClientEvent('def_crafting:getbow', _source)
+		  
+          Citizen.Wait(35000)
+		  
+                data.addItem(_source, name, 100, GetHashKey(weapon))
+                TriggerClientEvent("redemrp_notification:start", source, "Bow successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 2 Wood and 3 String', 5)
+        end
+    end)
+end)
+
+RegisterServerEvent('def_crafting:getknife')
+AddEventHandler("def_crafting:getknife", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "handle")
+        local count2 = data.checkItem(_source, "knifeblade")
+        local count3 = data.checkItem(_source, "string")
+		if count >= 1 and count2 >= 1 and count3 >= 1 then
+		
+			data.delItem(_source,"handle", 1)
+            data.delItem(_source,"knifeblade",1)
+            data.delItem(_source,"string",1)
+			
+          TriggerClientEvent('def_crafting:getknife', _source)
+		  
+          Citizen.Wait(35000)
+		  
+                data.addItem(_source, name, 100, GetHashKey(weapon))
+                TriggerClientEvent("redemrp_notification:start", source, "Knife successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 1 Handle, 1 Knifeblade and 1 String', 5)
+        end
+    end)
+end)
+
+RegisterServerEvent('def_crafting:getthrowing')
+AddEventHandler("def_crafting:getthrowing", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "handle")
+        local count2 = data.checkItem(_source, "knifeblade")
+        local count3 = data.checkItem(_source, "string")
+		if count >= 1 and count2 >= 1 and count3 >= 1 then
+		
+			data.delItem(_source,"handle", 1)
+            data.delItem(_source,"knifeblade",1)
+            data.delItem(_source,"string",1)
+			
+          TriggerClientEvent('def_crafting:getthrowing', _source)
+		  
+          Citizen.Wait(35000)
+		  
+                data.addItem(_source, name, 100, GetHashKey(weapon))
+                TriggerClientEvent("redemrp_notification:start", source, "Throwing knife successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 1 Handle, 1 Knifeblade and 1 String', 5)
+        end
+    end)
+end)
+
+-- Gegenstände
+
+RegisterServerEvent('def_crafting:getknifeblade')
+AddEventHandler("def_crafting:getknifeblade", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "iron")
+		if count >= 3 then
+		
+			data.delItem(_source,"iron", 3)
+			
+          TriggerClientEvent('def_crafting:getknifeblade', _source)
+		  
+          Citizen.Wait(32500)
+		  
+                data.addItem(_source,"knifeblade", 1)
+                TriggerClientEvent("redemrp_notification:start", source, "Knifeblade successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 3 Iron', 5)
+        end
+    end)
+end)
+
+RegisterServerEvent('def_crafting:gethandle')
+AddEventHandler("def_crafting:getghandle", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "stick")
+		if count >= 1 then
+		
+			data.delItem(_source,"stick", 1)
+			
+          TriggerClientEvent('def_crafting:gethandle', _source)
+		  
+          Citizen.Wait(25000)
+		  
+                data.addItem(_source,"handle", 1)
+                TriggerClientEvent("redemrp_notification:start", source, "Handle successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 1 Stick', 5)
+        end
+    end)
+end)
+
+RegisterServerEvent('def_crafting:getfishinghook')
+AddEventHandler("def_crafting:getfishinghook", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "iron")
+		if count >= 1 then
+		
+			data.delItem(_source,"iron", 1)
+			
+          TriggerClientEvent('def_crafting:getfishinghook', _source)
+		  
+          Citizen.Wait(30000)
+		  
+                data.addItem(_source, "fishinghook", 3)
+                TriggerClientEvent("redemrp_notification:start", source, "Fishinghook successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 1 Iron', 5)
+        end
+    end)
+end)
+
+RegisterServerEvent('def_crafting:getarrowhead')
+AddEventHandler("def_crafting:getarrowhead", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "iron")
+		if count >= 1 then
+		
+			data.delItem(_source,"iron", 1)
+			
+          TriggerClientEvent('def_crafting:getarrowhead', _source)
+		  
+          Citizen.Wait(30000)
+		  
+                data.addItem(_source, "arrowhead", 1)
+                TriggerClientEvent("redemrp_notification:start", source, "Arrowhead successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 1 Iron', 5)
+        end
+    end)
+end)
+
+-- Werkzeug
+
+RegisterServerEvent('def_crafting:getfishingrod')
+AddEventHandler("def_crafting:getfishingrod", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local count = data.checkItem(_source, "stick")
+        local count2 = data.checkItem(_source, "fishinghook")
+        local count3 = data.checkItem(_source, "string")
+		if count >= 3 and count2 >= 1 and count3 >= 2 then
+		
+			data.delItem(_source,"stick", 3)
+            data.delItem(_source,"fishinghook",1)
+            data.delItem(_source,"string",2)
+			
+          TriggerClientEvent('def_crafting:getfishingrod', _source)
+		  
+          Citizen.Wait(30000)
+		  
+                data.addItem(_source, "fishingrod", 1)
+                TriggerClientEvent("redemrp_notification:start", source, "Fishingrod successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 3 Stick, 1 Fishinghook und 2 String', 5)
+        end
+    end)
+end)
+
+--- Munition
+
+RegisterServerEvent('def_crafting:getarrow')
+AddEventHandler("def_crafting:getarrow", function(name, weapon)
+    local _source = tonumber(source)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+        local count = data.checkItem(_source, "stick")
+        local count2 = data.checkItem(_source, "feather")
+        local count3 = data.checkItem(_source, "arrowhead")
+		if count >= 1 and count2 >= 4 and count3 >= 1 then
+		
+            data.delItem(_source,"stick", 1)
+            data.delItem(_source,"feather", 4)
+            data.delItem(_source,"arrowhead", 1)
+			
+          TriggerClientEvent('def_crafting:getarrow', _source)
+		  
+          Citizen.Wait(31000)
+		  
+                data.addItem(_source, "arrows", 1)
+                TriggerClientEvent("redemrp_notification:start", source, "Arrow successfully produced", 2, "success")
+        else
+            TriggerClientEvent("redemrp_notification:start", _source, 'You need: 1 Stick, 4 Feather und 1 Arrowhead', 4)
+        end
+    end)
+end)
+
